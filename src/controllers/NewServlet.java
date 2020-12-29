45c2e7be443c2e29ef6e694d0ce7b1e52ef77153
Satoru.Takemoto <s.takemoto.527@gmail.com>
2020-12-29 12:34:44 +0900

    Add CreateServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
 
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
@@ -22,6 +20,7 @@ public class NewServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*
         EntityManager em = DBUtil.createEntityManager();
         em.getTransaction().begin();
 
@@ -45,7 +44,15 @@ public class NewServlet extends HttpServlet {
 
         //自動採番されたIDの値を表示
         response.getWriter().append(Integer.valueOf(m.getId()).toString());
+        */
 
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
+
+        //インスタンスを生成
+        request.setAttribute("message", new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
     }
-
 }
